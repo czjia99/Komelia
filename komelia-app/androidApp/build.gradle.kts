66f77a66e6a386d780f6db6ad883c52a6d92f198
Snd-R <76580768+snd-r@users.noreply.github.com>
2026-09-01 20:14:41 +0300

    playstore obfuscation

--- komelia-app/androidApp/build.gradle.kts
+++ komelia-app/androidApp/build.gradle.kts
@@ -11,7 +11,7 @@ group = "io.github.snd-r.komelia"
 version = libs.versions.app.version.get()
 
 
-dependencies{
+dependencies {
     implementation(projects.komeliaApp.shared)
     implementation(projects.komeliaUi)
     implementation(projects.komeliaDomain.core)
@@ -98,9 +98,20 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "android.pro"
+
+            // required by playstore. helps to reduce apk/bundle size
+            // saves 185KB but mangles stacktraces
+            val obfuscationRule = when (androidVariant) {
+                AndroidVariant.STANDALONE, AndroidVariant.FDROID -> "dont_obfuscate.pro"
+                AndroidVariant.PLAY -> null
+            }
+
+            setProguardFiles(
+                listOfNotNull(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    obfuscationRule,
+                    "android.pro",
+                )
             )
         }
     }
