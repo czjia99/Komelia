1ac6a4b61a366d901d957c6ed0b908f745c09989
Snd-R <76580768+snd-r@users.noreply.github.com>
2026-09-01 17:32:05 +0300

    version 0.19.0

--- komelia-app/androidApp/build.gradle.kts
+++ komelia-app/androidApp/build.gradle.kts
@@ -72,7 +72,7 @@ android {
         applicationId = "io.github.snd_r.komelia"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 20
+        versionCode = libs.versions.android.versionCode.get().toInt()
         versionName = libs.versions.app.version.get()
 
         val enableSelfUpdates = when (androidVariant) {
